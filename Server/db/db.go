aa151cec5354c097424c1e06d952b7edbc4b8563
Sam Al Jaboury <saljaboury1@st.noorderpoort.nl>
2026-07-13 22:10:41 +0200

    added concurrency via mutex protection, also added testing for concurrency

--- Server/db/db.go
+++ Server/db/db.go
@@ -1,10 +1,14 @@
 package db
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 // basic Gredis db
 type Db struct {
 	data map[string]string
+	mu   sync.Mutex
 }
 
 func NewGredisDb() *Db {
@@ -13,15 +17,20 @@ func NewGredisDb() *Db {
 
 // Set db data
 func (db *Db) Set(key string, value string) (bool, error) {
+
+	db.mu.Lock()
 	db.data[key] = value
+	db.mu.Unlock()
 
 	return true, nil
 }
 
 // Get cached db data
 func (db *Db) Get(key string) (string, error) {
+
 	if value, exists := db.data[key]; exists {
 		return value, nil
 	}
+
 	return "", errors.New("Key doesn't exist in the database")
 }
